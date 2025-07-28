b7c84118090ca7f0dba2f9d49c905e5afa0772ee
klu2300031372 <2300031372@kluniversity.in>
2025-07-28 10:07:05 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,3 @@ export default defineConfig({
   base: '/cicdproject/',
   plugins: [react()],
 });
-c. Update package.json
-"homepage": "https://klu2300031372.github.io/cicdproject/",
-"scripts": {
-  "lint": "eslint .",
-  "dev": "vite",
-  "build": "vite build",
-  "preview": "vite preview",
-  "predeploy": "npm run build",
-  "deploy": "gh-pages -d dist"
-}
